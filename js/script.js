a20291e5294706904196ee2f5b50378458ceb54f
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-25 02:02:34 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Osmond
+// Created on: april 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function updates a slider value.
+ */
+function myButtonClicked () {
+  const choice = parseInt(document.getElementById('guessed-number').value)
+  console.log(choice)
+}
+  //if (Plain Burger) {
+  //  console.log("Plain Burger") 
+    //document.getElementById('').innerHTML = ''
+  //} else {
+    //document.getElementById('').innerHTML = ''
+  //}
+
