e83144d5a5183be4a0055f012162dfae3a74ee90
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-26 02:13:21 +0000

    finished js code

--- js/script.js
+++ js/script.js
@@ -8,11 +8,76 @@
 
 function myButtonClicked () {
   const burger = document.getElementById('burger').value
-  console.log(burger)
-}
-  if (Plain) {
-    console.log("Plain Burger") 
-    document.getElementById('').innerHTML = ''
-  } else {
-    document.getElementById('').innerHTML = ''
+  const toppings = document.getElementById('toppings').value
+  //console.log(burger)
+  //console.log(toppings)
+
+  let total=0;
+  let calctax=0;
+  if (burger=="Plain Burger") 
+  {
+    //console.log("Plain Burger1") 
+    let plainBurger = 4.00
+    if (toppings=="1 Topping")
+    {
+      let onetopping
+      onetopping = 0.50
+      total = plainBurger + onetopping
+      calctax = total * 0.13    
+      total = total + calctax
+      document.getElementById('answer').innerHTML = "The cost is $" + total.toFixed(2) + " for a " + burger + " with 1 topping"
+    }  
+    if (toppings=="2 Topping")
+    {
+      let twotoppings
+      twotoppings = 1.00
+      total = plainBurger + twotoppings
+      calctax = total * 0.13    
+      total = total + calctax
+      document.getElementById('answer').innerHTML = "The cost is $" + total.toFixed(2) + " for a " + burger + " with 2 toppings"
+    } 
+    if (toppings=="3 Topping")
+    {
+      let threetoppings
+      threetoppings = 1.50
+      total = plainBurger + threetoppings
+      calctax = total * 0.13    
+      total = total + calctax
+      document.getElementById('answer').innerHTML = "The cost is $" + total.toFixed(2) + " for a " + burger + " with 3 toppings"
+    }
+  } 
+  else 
+  {    
+    //console.log("Cheeseburger") 
+    let chburger = 5.00    
+    if (toppings=="1 Topping")
+    {
+      let onetopping
+      onetopping = 0.50
+      total = chburger + onetopping
+      calctax = total * 0.13    
+      total = total + calctax
+      document.getElementById('answer').innerHTML = "The cost is $" + total.toFixed(2) + " for a " + burger + " with 1 topping"
+    }  
+    if (toppings=="2 Topping")
+    {
+      let twotoppings
+      twotoppings = 1.00
+      total = chburger + twotoppings
+      calctax = total * 0.13    
+      total = total + calctax
+      document.getElementById('answer').innerHTML = "The cost is $" + total.toFixed(2) + " for a " + burger + " with 2 toppings"
+    } 
+    if (toppings=="3 Topping")
+    {
+      let threetoppings
+      threetoppings = 1.50
+      total = chburger + threetoppings
+      calctax = total * 0.13    
+      total = total + calctax
+      document.getElementById('answer').innerHTML = "The cost is $" + total.toFixed(2) + " for a " + burger + " with 3 toppings"
+    }
   }
+  //console.log(total)
+  //document.getElementById('answer').innerHTML = "The cost is $" + total.toFixed(2)
+}
